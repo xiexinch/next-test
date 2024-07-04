d601a2d929b38c82a9b3471c26b77f219a002afd
xiexinch <xiexinch@outlook.com>
2024-07-04 23:07:02 +0800

    --update

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -8,6 +8,7 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { invoices } from './placeholder-data';
 
 export async function fetchRevenue() {
   try {
@@ -158,6 +159,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    console.log(invoice)
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
